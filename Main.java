ae45ef82045caf7464bf527259d86da9ff2ae140
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-7.local>
2019-02-05 11:18:52 -0500

    Solved problem. Greet Alice and Bob and tell off everyone else

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
-    public static void main(String[] args ){
-
-    }
+  public static void main(){//String[] args ){
+      String nameString; 
+      Scanner nameInput = new Scanner(System.in);
+      System.out.println("Hi, friend! What's your name?"); 
+      nameString = nameInput.nextLine();
+      if (nameString.equals("Alice") || nameString.equals("Bob")){
+      System.out.println("Greetings,  "+ nameString);
+     } else {
+         System.out.println("Soory,  "+ nameString + ", only Alice and Bob allowed");
+        }
+  }  
 }
